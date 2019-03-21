36c50cacf24a48b4e391f00bbed1e5b9f825cfb8
BarreiroLeo <a.leo.barreiro@gmail.com>
2019-03-21 15:23:46 -0300

    update main.py

--- main.py
+++ main.py
@@ -1,26 +1,25 @@
-import serial
-import time
-# import pdb
-# pdb.set_trace()
-
-# Listar dispositivos en terminal:
-# ls /dev/ttyAC*
-# arduino = serial.Serial('/dev/ttyACM0', 115200, timeout=2)
-# arduino.close()
-
 """
 El script debe:
+
 Conectarse con el micro.
-Enviar data hasta que responda "ok"
 
 Una vez que está en espera el micro, preguntar:
   Margenes de escaneo: [{x_inf_izq,y_inf_izq},{x2,y2},{x3,y3},{xf,yf}]
   Cantidad de puntos de escaneo: [{x_cant},{y_cant}]
+
 Con los datos procesar:
   Dividir la distancia máxima de cada eje entre la cantidad de puntos a sensar
   El valor obtenido es el avance que hay que darle al código G (¿Coord relat?)
+
+Posicionarse en cero automáticamente y establecer sus coordenadas Z en cero
 """
 
+import serial
+import time
+# import pdb
+# pdb.set_trace()
+# Listar dispositivos en terminal:
+# ls /dev/ttyAC*
 # Instancia de objeto serial
 ard_serial = serial.Serial('/dev/ttyACM0', 115200, timeout=0.5)
 
@@ -28,25 +27,24 @@ ard_serial = serial.Serial('/dev/ttyACM0', 115200, timeout=0.5)
 def input_datos():
     print("Margenes de escaneo:")
     # x_inf_izq = float(input("X inferior izq: "))
-    x_inf_izq = 0
     # y_inf_izq = float(input("Y inferior izq: "))
-    y_inf_izq = 0
     # x_sup_der = float(input("X superior der: "))
-    x_sup_der = 10
     # y_sup_der = float(input("Y superior der: "))
-    y_sup_der = 10
 
     # Imprimir posiciones para verificar la entrada de datos
-    tupla_posiciones = (x_inf_izq, y_inf_izq, x_sup_der, y_sup_der)
-    print("TuplaPosiciones: " + str(tupla_posiciones))
     # n_puntos_x = float(input("Cantidad de puntos en X: "))
-    n_puntos_x = 2
     # n_puntos_y = float(input("Cantidad de puntos en Y: "))
+    x_inf_izq = 0
+    y_inf_izq = 0
+    x_sup_der = 10
+    y_sup_der = 10
+    n_puntos_x = 2
     n_puntos_y = 2
-
     avance_x = (x_sup_der - x_inf_izq) / n_puntos_x
     avance_y = (y_sup_der - y_inf_izq) / n_puntos_y
 
+    tupla_posiciones = (x_inf_izq, y_inf_izq, x_sup_der, y_sup_der)
+    print("TuplaPosiciones: " + str(tupla_posiciones))
     print("Avance X: " + str(avance_x))
     print("Avance Y: " + str(avance_y))
 
@@ -60,15 +58,26 @@ def wake_up_grbl():
 
 
 def send_to_serial(gcode):
-    command_to_send = gcode
-    print("\r\n" + command_to_send)
+    """
+    Recibe un array con dos elementos:
+    Elemento 1: Gcode
+    Elemento 2: Tipo de mensaje de retorno
+    """
+
+    print(gcode[0])
+    command_to_send = gcode[0] + "\r\n"
     ard_serial.write(bytes(command_to_send, encoding="ascii"))
     while True:
         grbl_says = str(ard_serial.readline())
-        if grbl_says != '':                 # Imprimir los mensajes no vacíos
-            print("Grbl says: " + grbl_says)
-        if grbl_says.find("") > (-1):     # Cuando encuentre ok, salir.
-            break
+        # if grbl_says != '':               # Imprimir los mensajes no vacíos
+        #    print("Grbl says: " + grbl_says)
+        if gcode[1] == 1:
+            if grbl_says.find("ok") > (-1):     # Cuando encuentre ok, salir.
+                break
+        if gcode[1] == 2:
+            if grbl_says.find("PRB") > (-1):    # Cuando encuentre PRB, salir.
+                print("Grbl says: " + grbl_says + "\n")
+                break
 
 
 def mapear():
@@ -80,31 +89,27 @@ def mapear():
         if grbl_says.find("''") > (-1):     # Si hay mensaje vacío (timeout)
             break                           # Salir del while
 
-    gcodes = ["G91",
-              "G1 Z1 F50",
-              "G38.2 Z-10 F50",
-              "G38.5 Z1 F50",
-              "G38.2 Z-1 F10",
-              "G38.5 Z1 F10",
-              "G92 Z0",
-              "$X",
-              "M2"
+    gcodes = [["G91", 1],             # Tupla de tuplas:
+              ["G1 Z1 F50", 1],       # 1er elemento: gcode
+              ["G38.2 Z-10 F50", 2],  # 2do elemento: tipo mensaje de retorno
+              ["G38.5 Z01 F50", 2],     # Tipo 1: "ok"
+              ["G38.2 Z-1 F10", 2],     # Tipo 2: "PRB"
+              ["G38.5 Z01 F10", 2],
+              ["G38.2 Z-1 F10", 2],
+              ["G38.5 Z01 F10", 2],
+              ["G38.2 Z-1 F10", 2],
+              ["G38.5 Z01 F10", 2],
+              # ["G92 Z0", 1],      # Establece coordenadas en Z = 0
+              # ["?", 1],
+              # ["G04 P5", 1],     # Pausa de 1seg
+              # ["?", 1],
+              ["$X", 1],
+              ["M2", 1]
               ]
 
     for i in range(len(gcodes)):
-        send_to_serial(gcodes[i] + "\r\n")
+        send_to_serial(gcodes[i])
 
-    """command_to_send = "G91 \r\n"
-    command_to_send = gcodes[0]
-    print("\r\n" + command_to_send)
-    ard_serial.write(bytes(command_to_send, encoding="ascii"))
-    while True:
-        grbl_says = str(ard_serial.readline())
-        if grbl_says != '':                 # Imprimir los mensajes no vacíos
-            print("Grbl says: " + grbl_says)
-        if grbl_says.find("ok") > (-1):     # Cuando encuentre ok, salir.
-            break
-    """
     return 1
 
 
